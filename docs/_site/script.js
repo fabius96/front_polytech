a8a24b88035ca9a1def349c69bb787e519ae1eed
KORJaeyoungKim <jy0511_@naver.com>
2023-05-31 09:48:02 +0900

    도넛 차트 색상 변경

--- docs/_site/script.js
+++ docs/_site/script.js
@@ -13,6 +13,9 @@ request.responseType = "json";
 request.onload = function () {
   var jsonData = request.response;
 
+  // 색상 배열을 직접 지정
+  var backgroundColors = ["#00857E", "#00B4A6", "#4DD0C8", "#F2A477", "#FF1A5C", "#FF4F80"];
+
   // 읽어들인 JSON 데이터를 바탕으로 새로운 Chart 객체 생성
   window.myChart = new Chart(ctx, {
     type: 'doughnut',
@@ -21,8 +24,8 @@ request.onload = function () {
       datasets: [{
         label: '5월 지출내역',
         data: jsonData.data,
-        backgroundColor: jsonData.backgroundColor,
-        borderColor: jsonData.borderColor,
+        backgroundColor: backgroundColors,
+        borderColor: 'white',
         borderWidth: 1
       }]
     },
